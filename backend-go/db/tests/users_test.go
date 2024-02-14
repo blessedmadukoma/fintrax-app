14bd6f3603e90272d6888a4366f872bdf3694a5b
blessedmadukoma <blessedmadukoma@gmail.com>
2024-02-14 21:11:37 +0100

    tests - update user

--- backend-go/db/tests/users_test.go
+++ backend-go/db/tests/users_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateUser(t *testing.T) {
+func createRandomUser(t *testing.T) db.User {
 	hashedPassword, err := utils.GenerateHashPassword(utils.RandomString(8))
 	if err != nil {
 		log.Fatal("Unable to generate password:", err)
@@ -31,9 +31,45 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.HashedPassword, arg.HashedPassword)
 
 	assert.WithinDuration(t, user.CreatedAt, time.Now(), 2*time.Second)
+	assert.WithinDuration(t, user.UpdatedAt, time.Now(), 2*time.Second)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	user1 := createRandomUser(t)
 
 	// create a new user using the same details and verify if the email is the same: it should return an error
-	user2, err := testQuery.CreateUser(context.Background(), arg)
+	user2, err := testQuery.CreateUser(context.Background(), db.CreateUserParams{
+		Email:          user1.Email,
+		HashedPassword: user1.HashedPassword,
+	})
+
 	assert.Error(t, err)
 	assert.Empty(t, user2)
+}
+
+func TestUpdateUser(t *testing.T) {
+	user := createRandomUser(t)
+
+	newPassword, err := utils.GenerateHashPassword(utils.RandomString(8))
+	if err != nil {
+		log.Fatal("unable to generate password:", err)
+	}
+
+	arg := db.UpdateUserPasswordParams{
+		HashedPassword: newPassword,
+		ID:             user.ID,
+		UpdatedAt:      time.Now(),
+	}
+
+	newUser, err := testQuery.UpdateUserPassword(context.Background(), arg)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, user)
+
+	assert.Equal(t, newUser.HashedPassword, arg.HashedPassword)
+	assert.Equal(t, user.Email, newUser.Email)
+
+	assert.WithinDuration(t, user.UpdatedAt, time.Now(), 2*time.Second)
 }
